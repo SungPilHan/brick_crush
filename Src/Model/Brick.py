07b36b85bfa523fdbb8c2130d4b47c0fd8be5518
sungpil han <ttt16541@gmail.com>
2018-05-12 13:00:39 +0900

    module develope

--- Src/Model/Brick.py
+++ Src/Model/Brick.py
@@ -1,20 +1,17 @@
-#!/usr/bin/python
 #-*- coding: utf-8 -*-
+from tkinter import *
 
 class Brick:
-    def __init__(self):
-        self.canvas = None
-        self.posX = None
-        self.posY = None
-        self.id = None
-        self.sizeX1 = None
-        self.sizeY1 = None
-        self.sizeX2 = None
-        self.sizeY2 = None
-
     def __init__(self, canvas , X , Y ):
-        pass
+        self.canvas = canvas
+        self.posX = X
+        self.posY = Y
+        self.sizeX1 = 0
+        self.sizeY1 = 0
+        self.sizeX2 = 50
+        self.sizeY2 = 25
+        self.id = self.canvas.create_rectangle(self.sizeX1,self.sizeY1,self.sizeX2,self.sizeY2,fill = 'red')
+        self.canvas.move(self.id, self.posX, self.posY)
 
-    def get_id(self, ):
-        pass
-
+    def get_id(self):
+        return self.id
--- Src/Model/Controller.py
+++ Src/Model/Controller.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+from tkinter import *
+
+class Brick:
+    def __init__(self, canvas , X , Y ):
+        self.canvas = canvas
+        self.posX = X
+        self.posY = Y
+        self.sizeX1 = 0
+        self.sizeY1 = 0
+        self.sizeX2 = 50
+        self.sizeY2 = 25
+        self.id = self.canvas.create_rectangle(self.sizeX1,self.sizeY1,self.sizeX2,self.sizeY2,fill = 'red')
+        self.canvas.move(self.id, self.posX, self.posY)
+
+    def get_id(self):
+        return self.id
 
 class Controller:
-    def __init__(self):
+    def __init__(self, tk):
+        self.canvas = Canvas(tk,width=500,height=400,bd=0,highlightthickness=0)
+        self.canvas.pack()
         self.ball = None
-        self.bricks = None
-        self.brick = None
+#        brick = Brick(self.canvas, 200, 300)
         self.bar = None
-        self.canvas = None
-
+        self.bricks = []
+        for i in range(0, 4):
+            for j in range(0, 5):
+                self.bricks.append(Brick(self.canvas, 55*j+120, 30*i+50))
+                              
     def conflict_ball_wall(self, ball_id):
         pass
 
@@ -23,4 +43,11 @@ class Controller:
 
     def draw_all(self, ball, bricks, bar, canvas):
         pass
+
+
+tk = Tk()
+tk.title("test")
+tk.wm_attributes("-topmost",1)
+cs = Controller(tk)
+tk.update()
 
